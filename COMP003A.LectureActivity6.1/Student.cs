f5607dab4e3ab55d515cdcb549d3d83d301b2620
Isaac Martinez <isaacmartinez2429@gmail.com>
2026-02-20 18:37:40 -0800

    created new class

---
+++ COMP003A.LectureActivity6.1/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.LectureActivity6._1
+{
+    public class Student
+    {
+       
+
+    }
+}
